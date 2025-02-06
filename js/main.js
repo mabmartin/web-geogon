244a6df118c6d27104385b820adeaa33982f782d
mabmartin <mab_martin@hotmail.com>
2025-02-06 00:59:23 -0300

    gallery

--- js/main.js
+++ js/main.js
@@ -90,25 +90,7 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1000,
-        center: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
+
+
 })(jQuery);
 
